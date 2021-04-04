d8cf64b6310c35b9fd92995daf65be24a69ff696
Slokh <slokh@octane.gg>
2021-04-04 14:57:35 -0400

    Display coaches and subs as participants

--- components/events/Participants.js
+++ components/events/Participants.js
@@ -1,18 +1,40 @@
-import { Flex, Stack, Text, Image, SimpleGrid, Spacer } from '@chakra-ui/core'
+import { Flex, Stack, Text, Image, SimpleGrid } from '@chakra-ui/core'
 import Flag from '@octane/components/common/Flag'
 import { Link } from '@octane/components/common/Text'
+
+const Player = ({ player }) => (
+  <Stack paddingLeft={4} direction="row" align="center">
+    <Flag country={player.country || 'int'} />
+    <Link href={`/players/${player._id}`}>
+      <Stack direction="row" align="center" spacing={1}>
+        <Text>{player.tag}</Text>
+        {player.substitute && (
+          <Text fontSize="11px" color="secondary.500" fontWeight="semi">
+            (S)
+          </Text>
+        )}
+        {player.coach && (
+          <Text fontSize="11px" color="secondary.500" fontWeight="semi">
+            (C)
+          </Text>
+        )}
+      </Stack>
+    </Link>
+  </Stack>
+)
 
 export const Participants = ({ participants }) => (
   <SimpleGrid columns={4} spacingY={8}>
     {participants?.map(({ team, players }, j) => {
-      const sortedPlayers = players.sort((a, b) => a.tag.localeCompare(b.tag))
-      const playerSetOne = sortedPlayers.slice(0, 3)
-      const playerSetTwo = sortedPlayers.slice(3)
+      const playerSetOne = players.slice(0, 5)
+      const playerSetTwo = players.slice(5)
 
       return (
         <Stack key={j} width="full" marginLeft={8}>
           <Stack direction="row" align="center">
-            <Text width={6} align="center" fontSize="11px" color="secondary.500" fontWeight="semi">{team.region}</Text>
+            <Text width={6} align="center" fontSize="11px" color="secondary.500" fontWeight="semi">
+              {team.region}
+            </Text>
             {team.image && <Image width={6} src={team.image} />}
             <Link href={`/teams/${team._id}`}>
               <Text>{team.name}</Text>
@@ -21,23 +43,13 @@ export const Participants = ({ participants }) => (
           <Flex paddingLeft={3}>
             <Stack borderLeft="2px solid #94e8be">
               {playerSetOne.map((player, i) => (
-                <Stack key={i} paddingLeft={4} direction="row" align="center">
-                  <Flag country={player.country || 'int'} />
-                  <Link href={`/players/${player._id}`}>
-                    <Text>{player.tag}</Text>
-                  </Link>
-                </Stack>
+                <Player key={i} player={player} />
               ))}
             </Stack>
             {playerSetTwo.length > 0 && (
               <Stack>
                 {playerSetTwo.map((player, i) => (
-                  <Stack key={i} paddingLeft={4} direction="row" align="center">
-                    <Flag country={player.country || 'int'} />
-                    <Link href={`/players/${player._id}`}>
-                      <Text>{player.tag}</Text>
-                    </Link>
-                  </Stack>
+                  <Player key={i} player={player} />
                 ))}
               </Stack>
             )}
