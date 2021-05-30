3ef392230995c791ecbf1158ad3c405dc3dc455f
Slokh <slokh@octane.gg>
2021-05-29 21:24:07 -0400

    Add show/hide players switch for participants

--- components/events/Participants.js
+++ components/events/Participants.js
@@ -1,12 +1,12 @@
-import { Flex, Stack, Text, Box, StackDivider } from '@chakra-ui/react'
-import Flag from '@octane/components/common/Flag'
+import { Flex, Stack, Text, Box, StackDivider, Switch } from '@chakra-ui/react'
 import Image from '@octane/components/common/Image'
 import { Link } from '@octane/components/common/Text'
+import { getCountry } from '@octane/config/fields/countries'
 import { useState } from 'react'
 
 const Player = ({ player }) => (
-  <Stack height="full" padding={1} paddingLeft={2} direction="row" align="center" spacing={1}>
-    <Flag country={player.country || 'int'} />
+  <Stack height="full" padding={0} paddingLeft={2} direction="row" align="center" spacing={1}>
+    <Image src={getCountry(player.country).image} boxSize={4} />
     <Link href={`/players/${player.slug}`} fontWeight="semi">
       <Stack direction="row" align="center" spacing={1}>
         <Text>{player.tag}</Text>
@@ -24,65 +24,77 @@ const Player = ({ player }) => (
   </Stack>
 )
 
-export const Participants = ({ participants }) => (
-  <Stack direction="row" spacing={0} padding={2} wrap="wrap" shouldWrapChildren>
-    {participants?.map(({ team, players }) => {
-      const [show, setShow] = useState(false)
-      return (
-        <Box
-          backgroundColor="secondary.25"
-          position="relative"
-          margin={3}
-          width={44}
-          height={44}
-          onMouseEnter={() => setShow(true)}
-          onMouseLeave={() => setShow(false)}>
-          <Stack
-            width="full"
-            height="full"
-            align="center"
-            justify="space-between"
-            position="absolute"
-            top={0}
-            left={0}
-            zIndex={0}>
-            <Image
-              src={team.image}
-              boxSize={32}
-              opacity={0.1}
-              display={{ base: 'flex', lg: 'none' }}
-            />
-            <Image
-              src={team.image}
-              boxSize={32}
-              opacity={show ? 0.05 : ''}
-              display={{ base: 'none', lg: 'flex' }}
-            />
-            <Flex
-              height={8}
-              width="full"
-              align="center"
-              justify="center"
-              borderTop="1px solid #BECCE4">
-              <Link href={`/teams/${team.slug}`}>{team.name}</Link>
-            </Flex>
-          </Stack>
-          <Stack
-            spacing={0}
-            divider={<StackDivider borderColor="secondary.200" />}
-            direction="column"
-            justify="space-around"
-            width={44}
-            height={36}
-            display={{ base: 'flex', lg: show ? 'flex' : 'none' }}>
-            {players.slice(0, 5).map((player, i) => (
-              <Player key={i} player={player} />
-            ))}
-          </Stack>
-        </Box>
-      )
-    })}
-  </Stack>
-)
+export const Participants = ({ participants }) => {
+  const [defaultShow, setDefaultShow] = useState(false)
+  return (
+    <Stack padding={2}>
+      <Stack direction="row" align="center" marginLeft={2}>
+        <Text fontSize="11px" fontWeight="bold" textTransform="uppercase" color="secondary.800">
+          Show/Hide
+        </Text>
+        <Switch isChecked={defaultShow} onChange={() => setDefaultShow(!defaultShow)} />
+      </Stack>
+      <Stack direction="row" spacing={0} wrap="wrap" shouldWrapChildren>
+        {participants?.map(({ team, players }) => {
+          const [show, setShow] = useState(false)
+
+          return (
+            <Box
+              backgroundColor="secondary.25"
+              position="relative"
+              margin={2}
+              width={40}
+              height={40}
+              onMouseEnter={() => setShow(true)}
+              onMouseLeave={() => setShow(false)}>
+              <Stack
+                width="full"
+                height="full"
+                align="center"
+                justify="space-between"
+                position="absolute"
+                top={0}
+                left={0}
+                zIndex={0}>
+                <Image
+                  src={team.image}
+                  boxSize={28}
+                  opacity={0.1}
+                  display={{ base: 'flex', lg: 'none' }}
+                />
+                <Image
+                  src={team.image}
+                  boxSize={28}
+                  opacity={show || defaultShow ? 0.05 : ''}
+                  display={{ base: 'none', lg: 'flex' }}
+                />
+                <Flex
+                  height={8}
+                  width="full"
+                  align="center"
+                  justify="center"
+                  borderTop="1px solid #BECCE4">
+                  <Link href={`/teams/${team.slug}`}>{team.name}</Link>
+                </Flex>
+              </Stack>
+              <Stack
+                spacing={0}
+                divider={<StackDivider borderColor="secondary.200" />}
+                direction="column"
+                justify="space-around"
+                width={40}
+                height={32}
+                display={{ base: 'flex', lg: show || defaultShow ? 'flex' : 'none' }}>
+                {players.slice(0, 5).map((player, i) => (
+                  <Player key={i} player={player} />
+                ))}
+              </Stack>
+            </Box>
+          )
+        })}
+      </Stack>
+    </Stack>
+  )
+}
 
 export default Participants
