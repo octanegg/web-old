387fe6ec5fb3ea0118a3518af85503022c21ce82
Slokh <slokhx@gmail.com>
2024-02-13 23:05:36 -0500

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '@octane/components/common/Layout'
-import Head from 'next/head'
-import Amplify from '@aws-amplify/core'
-import Auth from '@aws-amplify/auth'
-import 'react-datepicker/dist/react-datepicker.css'
-import '@octane/styles/date-picker.css'
-import OctaneProvider from '@octane/context/octane'
-import theme from '@octane/config/theme/theme'
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "@octane/components/common/Layout";
+import Head from "next/head";
+// import Amplify from '@aws-amplify/core'
+// import Auth from '@aws-amplify/auth'
+import "react-datepicker/dist/react-datepicker.css";
+import "@octane/styles/date-picker.css";
+import OctaneProvider from "@octane/context/octane";
+import theme from "@octane/config/theme/theme";
 
 // Amplify.configure({
 //   Auth: {
@@ -34,31 +34,34 @@ import theme from '@octane/config/theme/theme'
 // })
 
 const App = ({ Component, pageProps }) => (
-  <OctaneProvider>
-    <ChakraProvider theme={theme}>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<OctaneProvider>
+		<ChakraProvider theme={theme}>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Rocket League News & Coverage | Octane.gg</title>
+				<title>Rocket League News & Coverage | Octane.gg</title>
 
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-        <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />
-        <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet" />
+				<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
+				<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />
+				<link
+					href="//cdn.quilljs.com/1.3.6/quill.bubble.css"
+					rel="stylesheet"
+				/>
 
-        <link href="//cdn.quilljs.com/1.3.6/quill.core.css" rel="stylesheet" />
-        <script src="//cdn.quilljs.com/1.3.6/quill.core.js" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
-  </OctaneProvider>
-)
+				<link href="//cdn.quilljs.com/1.3.6/quill.core.css" rel="stylesheet" />
+				<script src="//cdn.quilljs.com/1.3.6/quill.core.js" />
+			</Head>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ChakraProvider>
+	</OctaneProvider>
+);
 
-export default App
+export default App;
--- util/auth.js
+++ util/auth.js
@@ -1,9 +1,16 @@
-import { createGetServerSideAuth, createUseAuth } from 'aws-cognito-next'
-import pems from '../pems.json'
+// import { createGetServerSideAuth, createUseAuth } from 'aws-cognito-next'
+// import pems from '../pems.json'
 
-export const getServerSideAuth = createGetServerSideAuth({ pems })
-export const useAuth = createUseAuth({ pems })
-export * from 'aws-cognito-next'
+// export const getServerSideAuth = createGetServerSideAuth({ pems })
+export const getServerSideAuth = () => {
+	return undefined;
+};
+// export const useAuth = createUseAuth({ pems })
+export const useAuth = () => {
+	return undefined;
+};
+// export * from 'aws-cognito-next'
 
 export const isAdmin = (auth) =>
-  auth?.accessTokenData && (auth.accessTokenData['cognito:groups'] || []).includes('admin')
+	auth?.accessTokenData &&
+	(auth.accessTokenData["cognito:groups"] || []).includes("admin");
